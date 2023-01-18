f3bd5f91af22e7295a7fd3f93d56c187ed946579
Paul Morgan <orbitingmind@gmail.com>
2023-01-17 23:21:55 -0500

    made some changes to css and readme file

--- routes/edit.js
+++ routes/edit.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const editController = require('../controllers/edit')
 
-router.get('/:id', editController.getEdit)
-router.get('/remove/:id', editController.deleteItem)
+router.get('/:id', editController.getEdit)  
+
+router.get('/remove/:id', editController.deleteItem) 
+
 router.post('/update/:id', editController.updateItem)
 
 
